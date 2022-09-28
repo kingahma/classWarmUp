eeb3c839d95c5e074b3158092de05ee5e26d176f
kingahma <zahid.king1999@gmail.com>
2022-09-28 18:23:18 -0400

    first commit

---
+++ src/stringProject/Main.java
@@ -0,0 +1,12 @@
+package stringProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// single line comment
+		String message = "Hello class";
+		System.out.println(message);
+
+	}
+
+}
